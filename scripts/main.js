0aefc042ceb3ebbd464c734063114433cc27c21d
RomainKH <romain.khanoyan@gmail.com>
2018-05-26 19:45:13 +0200

    ajout images
    ajout des images

--- scripts/main.js
+++ scripts/main.js
@@ -1,38 +1,44 @@
-let laVille = document.querySelector('.laVille'), laNature = document.querySelector('.laNature'), lArchitect = document.querySelector('.lArchitect'), lesCuriosites = document.querySelector('.lesCuriosites')
+let natures = document.querySelector('.natures'), ville = document.querySelector('.ville'), danse = document.querySelector('.danse')
 
-laVille.addEventListener(
+natures.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
     for (var i = 0; i < img.length; i++) {
-      img[i].setAttribute('src',`../images/laville${i}.png`)
+      img[i].setAttribute('src',``)
+      img[i].setAttribute('alt',``)
+    }
+    for (var j = 0; j < 12; j++) {
+      img[j].setAttribute('src',`../images/natures${j}.jpg`)
+      img[j].setAttribute('alt',`peinture aquarelle`)      
     }
   }
 )
-laNature.addEventListener(
+ville.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
     for (var i = 0; i < img.length; i++) {
-      img[i].setAttribute('src',`../images/lanature${i}.png`)
+      img[i].setAttribute('src',``)
+      img[i].setAttribute('alt',``)
+    }
+    for (var j = 0; j < 4; j++) {
+      img[j].setAttribute('src',`../images/ville${j}.jpg`)
+      img[j].setAttribute('alt',`peinture aquarelle`)
     }
   }
 )
-lArchitect.addEventListener(
+danse.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
     for (var i = 0; i < img.length; i++) {
-      img[i].setAttribute('src',`../images/larchitect${i}.png`)
+      img[i].setAttribute('src',``)
+      img[i].setAttribute('alt',``)
     }
-  }
-)
-lesCuriosites.addEventListener(
-  "click",
-  function(){
-    let img = document.querySelectorAll('.imagesAquarelle img')
-    for (var i = 0; i < img.length; i++) {
-      img[i].setAttribute('src',`../images/lescurio${i}.png`)
+    for (var j = 0; j < 7; j++) {
+      img[j].setAttribute('src',`../images/danse${j}.jpg`)
+      img[j].setAttribute('alt',`peinture aquarelle`)
     }
   }
 )
