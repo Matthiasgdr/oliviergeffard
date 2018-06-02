c92845fe7be526f550b2ddca54647bae17c27988
RomainMaillot <32742569+RomainMaillot@users.noreply.github.com>
2018-06-02 12:38:19 +0200

    Mise en place à la bonne échelle des images

--- scripts/main.js
+++ scripts/main.js
@@ -1,11 +1,11 @@
 let natures = document.querySelector('.natures'), ville = document.querySelector('.ville'), danse = document.querySelector('.danse')
-
 natures.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
-    let trois = document.querySelectorAll('.imagesAquarelle .trois')
     let naturesNames = oeuvres.natures
+    let current = document.querySelector('.current')
+    let divs = document.querySelectorAll('.empty')
     for (var i = 0; i < img.length; i++) {
       img[i].setAttribute('src',``)
       img[i].setAttribute('alt',``)
@@ -15,26 +15,37 @@ natures.addEventListener(
     for (var j = 0; j < 12; j++) {
       img[j].setAttribute('src',`../images/natures${j}.jpg`)
       img[j].setAttribute('alt',`peinture aquarelle`)
-    }})
+    }
+    for (var k = 0; k < divs.length; k++) {
+      divs[k].classList.remove('empty')
+    }
+    img[3].style.height = '450px'
+    img[4].style.height = '450px'
+    img[5].style.height = '450px'
+    current.classList.remove('current')
+    natures.firstChild.classList.add('current')
+  })
 
 ville.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
-    let trois = document.querySelectorAll('.imagesAquarelle .trois')
     let villeNames = oeuvres.ville
+    let current = document.querySelector('.current')
+    let lines = document.querySelectorAll('.line')
     for (var i = 0; i < img.length; i++) {
       img[i].setAttribute('src',``)
       img[i].setAttribute('alt',``)
       img[i].setAttribute('class',``)
     }
     for (var j = 0; j < 4; j++) {
-      img[0].setAttribute('class',`bigger`)
       img[j].setAttribute('src',`../images/ville${j}.jpg`)
       img[j].setAttribute('alt',`peinture aquarelle`)
     }
-    for (var k = 4; k < 13; k++) {
-    }
+    lines[2].classList.add('empty')
+    lines[3].classList.add('empty')
+    current.classList.remove('current')
+    ville.firstChild.classList.add('current')
   })
 
 
@@ -42,19 +53,24 @@ danse.addEventListener(
   "click",
   function(){
     let img = document.querySelectorAll('.imagesAquarelle img')
-    let trois = document.querySelectorAll('.imagesAquarelle .trois')
     let danseNames = oeuvres.danse
+    let current = document.querySelector('.current')
+    let lines = document.querySelectorAll('.line')
     for (var i = 0; i < img.length; i++) {
       img[i].setAttribute('src',``)
       img[i].setAttribute('alt',``)
       img[i].setAttribute('class',``)
     }
     for (var j = 0; j < 7; j++) {
-      trois[j].setAttribute('class',``)
-      trois[j].setAttribute('class',`trois`)
       img[j].setAttribute('src',`../images/danse${j}.jpg`)
       img[j].setAttribute('alt',`peinture aquarelle`)
     }
-    for (var k = 7; k < 13; k++) {
-
-    }})
+    if (lines[2].classList.contains('empty')) {
+      lines[2].classList.remove('empty')
+    }
+    img[3].style.height = '400px'
+    img[4].style.height = '400px'
+    img[5].style.height = '400px'
+    lines[3].classList.add('empty')
+    current.classList.remove('current')
+    danse.firstChild.classList.add('current')})
